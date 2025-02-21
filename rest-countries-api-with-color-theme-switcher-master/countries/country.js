c4284801d336fa79b0aa8bbd2766028480cf7120
sushilsahoo06 <sahoosushil456@gmail.com>
2025-02-21 14:28:39 +0530

    new commit

--- rest-countries-api-with-color-theme-switcher-master/countries/country.js
+++ rest-countries-api-with-color-theme-switcher-master/countries/country.js
@@ -1,32 +1,30 @@
-const countryName= new URLSearchParams(window.location.search).get('name')
-const flagImg=document.querySelector(".country-daitels-img")
-const countryNameH1=document.querySelector('h1')
+const countryName = new URLSearchParams(window.location.search).get('name');
+const flagImg = document.querySelector(".country-daitels-img");
+const countryNameH1 = document.querySelector('h1');
+const detailsText = document.querySelector(".detail-text");
 
 fetch("https://restcountries.com/v3.1/all")
-  .then((req)=>req.json())
-  .then((data)=>{
-    console.log(data)
-    const country=data.find(c=>c.name.common==countryName)
-    if(country){
-      flagImg.src=country.flags.svg;
-      countryNameH1.innerText=country.name.common;
-
-      // const detailsText=document.createElement("div");
-      // detailsText.classList.add("detail-text");
-      // detailsText.innerHTML=`
-      //   <p><b>Native Name:</b> ${country.name.common}</p>
-      //   <p><b>Population:</b> ${country.population.toLocaleString("en-IN")}</p>
-      //   <p><b>Region:</b> ${country.region}</p>
-      //   <p><b>Sub Region:</b> ${country.region}</p>
-      //   <p><b>Capital:</b> ${country.capital}</p>
-      //   <p><b>Top Level Dominance:</b> ${country.timezones}</p>
-      //   <p><b>Currencies:</b> ${country.currencies}</p>
-      //   <p><b>Language:</b> ${country.language}</p>
-      //   `
-    }else{
-      console.log("country not found")
-    }
+  .then((req) => req.json())
+  .then((data) => {
+    console.log(data[0]);
+    const country = data.find(c => c.name.common === countryName);
     
-    // countryContainer.append(detailsText)
+    if (country) {
+      flagImg.src = country.flags.svg;
+      countryNameH1.innerText = country.name.common;
+
+      detailsText.innerHTML = `
+        <p><b>Native Name:</b> ${Object.values(country.name.nativeName)[0].common}</p>
+        <p><b>Population:</b> ${country.population.toLocaleString("en-IN")}</p>
+        <p><b>Region:</b> ${country.region}</p>
+        <p><b>Sub Region:</b> ${country.subregion}</p>
+        <p><b>Capital:</b> ${country.capital?.join(", ") || "N/A"}</p>
+        <p><b>Time Zone:</b> ${country.timezones.join(", ")}</p>
+        <p><b>Currencies:</b> ${Object.values(country.currencies)[0].name}</p>
+        <p><b>Language:</b> ${Object.values(country.languages).join(", ")}</p>
+      `;
+    } else {
+      console.log("Country not found");
+    }
   })
-  .catch((e)=>console.log(e))
+  .catch((e) => console.log(e));
