bc16e51209ab0fd6d49192be44ed23ae779fdde6
sushilsahoo06 <sahoosushil456@gmail.com>
2025-02-22 14:45:53 +0530

    new commit

---
+++ my-app/comonents/SearchBar.jsx
@@ -0,0 +1,12 @@
+import "../style/style.css"
+export default function SearchBar() {
+  return (
+    <div className="input-group flex-nowrap">
+      <span className="input-group-text" id="addon-wrapping">
+        <i className="fa-solid fa-magnifying-glass" ></i>
+      </span>
+        <input type="text" className="form-control" placeholder="Search for a country.." aria-label="Username" aria-describedby="addon-wrapping"/>
+      
+    </div>
+  );
+}
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "../comonents/Header";
+import SearchBar from "../comonents/SearchBar";
 
 export default function App() {
   return (
     <>
       <Header/>
+      <SearchBar/>
     </>
   )
 }
