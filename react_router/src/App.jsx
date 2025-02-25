543e81d829bd2b4499e8207c197ad24c2f8963b1
sushilsahoo06 <sahoosushil456@gmail.com>
2025-02-25 19:33:30 +0530

    new commit

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -1,9 +1,25 @@
-
-import React from 'react'
+import './index.css'
+import Navbar from './components/Navbar'
+import { Routes ,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Products from './pages/Products'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <div className='container'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/products' element={<Products/>}/>
+      </Routes>
+      </div> 
+    </div>
+
   )
 }
 
