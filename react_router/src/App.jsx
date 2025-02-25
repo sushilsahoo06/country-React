039bc17ffcdbe57c4ad7848fb347e9d849c8d9b0
sushilsahoo06 <sahoosushil456@gmail.com>
2025-02-25 19:55:33 +0530

    new commit

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -1,24 +1,25 @@
 import './index.css'
-import Navbar from './components/Navbar'
-import { Routes ,Route } from 'react-router-dom'
+
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Products from './pages/Products'
+import RootLayout from './layout/RootLayout'
 
 const App = () => {
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>
+        <Route index element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='products' element={<Products/>}/>
+      </Route>
+    )
+  )
   return (
-    <div>
-      <Navbar />
-      <div className='container'>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/products' element={<Products/>}/>
-      </Routes>
-      </div> 
-    </div>
+    <RouterProvider router={router}/>
 
   )
 }
