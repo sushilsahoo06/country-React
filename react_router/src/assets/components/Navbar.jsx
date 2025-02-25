950a01b7c6e5181d4d5a032a444a789166ddd3c4
sushilsahoo06 <sahoosushil456@gmail.com>
2025-02-25 12:59:48 +0530

    new commit

---
+++ react_router/src/assets/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Product</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <button>Get Started</button>
+    </div>
+  )
+}
+
+export default Navbar
