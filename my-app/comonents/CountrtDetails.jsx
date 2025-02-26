f034ff5fb6b83db07f019876bb8d1c61f37ed9a9
sushilsahoo06 <sahoosushil456@gmail.com>
2025-02-26 22:06:03 +0530

    new commit

--- my-app/comonents/CountrtDetails.jsx
+++ my-app/comonents/CountrtDetails.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
-import Country from "../pages/Country";
+
 
 export const CountrtDetails = () => {
+  const countryName = new URLSearchParams(window.location.search).get('name');
   const[countries,setCountries]=useState([]);
-    const [error ,setError]=useState(null);
+  const [error ,setError]=useState(null);
   useEffect(()=>{
     fetch("https://restcountries.com/v3.1/all")
     .then((res)=>{
@@ -12,21 +13,51 @@ export const CountrtDetails = () => {
       }
       return res.json()
     })
-    .then((data)=>{
-      setCountries(data)
+    .then(([data])=>{
+      console.log(data)
+      const country = data.find(c => c.name.common === countryName);      
+      setCountries({
+        key:country.name.common,
+        NativeName:Object.values(country.name.nativeName[0].common),
+        population:country.population.toLocalString("en-IN"),
+        Region:country.subregion,
+        Capital:country.capital,
+        TimeZone:country.timezones,
+        Currencies:Object.values(country.currencies)[0].name,
+        Language:Object.values(country.languages),
+        image:country.flags.svg
+
+      })
     })
     .catch((error)=>setError(error.meassage))
-  })
+  },[])
   if(error){
     return <p>Error:{error}</p>
   }
   return (
-    countries.map((country)=>(
-      <Country
-        key={country.name.common}
-        NativeName={country.name.NativeName[0].common}
-
-      />
-    ))
+    <main>
+      <button className="back-button" onClick={() => window.history.back()}>
+        <i className="fa-solid fa-arrow-left"></i>&nbsp;&nbsp;Back
+      </button>
+      <div className="country-details">
+        <img src={countries.svg} alt={countries.key} className="country-details-img" />
+        <div className="detail-text-container">
+          <h1>Iceland</h1>
+          <div className="detail-text">
+            <p><b>Native Name:</b> {countries.NativeName}</p>
+            <p><b>Population:</b> {countries.population}</p>
+            <p><b>Region:</b> {countries.Region}</p>
+            <p><b>Capital:</b>{countries.Capital}</p>
+            <p><b>Time Zone:</b>{countries.TimeZone}</p>
+            <p><b>Currencies:</b>{countries.Currencies}</p>
+            <p><b>Language:</b> {countries.Language}</p>
+          </div>
+          <div className="border-country">
+            <p><b>Border Countries:</b> None (Island Nation)</p>
+          </div>
+        </div>
+      </div>
+    </main>
+      
   )
 }
