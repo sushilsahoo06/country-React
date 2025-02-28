f343a7ba5dd9b36d57a0a64cc8b2fecceb2f6566
sushilsahoo06 <sahoosushil456@gmail.com>
2025-02-28 23:01:55 +0530

    new commit

--- my-app/src/layout/ShimmerEffect.jsx
+++ my-app/src/layout/ShimmerEffect.jsx
@@ -2,7 +2,45 @@
 export const ShimmerEffect = () => {
   return (
     <div className="countries-container">
-
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
+      <div className="country-card"></div>
     </div>
   )
 }
