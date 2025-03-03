4ff417e93f8bdfee904f75a8335dcccf7896b233
sushilsahoo06 <sahoosushil456@gmail.com>
2025-03-04 00:36:37 +0530

    new commit

--- my-app/comonents/Header.jsx
+++ my-app/comonents/Header.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react"
 import "../style/style.css"
 const Header=()=> {
+  const [isDark ,setIsDark]=useState(()=>{
+    return localStorage.getItem("theme") === "dark"
+  });
+  useEffect(()=>{
+    if(isDark){
+      document.body.classList.add("dark")
+    }else{
+      document.body.classList.remove("dark")
+    }
+    localStorage.setItem("theme" , isDark ? "dark" : "light");
+  },[isDark]);
   return (
     <header className="header">
     <div className="header-content">
       <h2 className="title">Where is the world?</h2>
-      <p><i className="fa-regular fa-moon"></i>&nbsp;&nbsp;Dark Mode</p>
+
+      <p className="theme-change" onClick={()=>
+      setIsDark((prev)=> !prev)
+      }><i className={`fa-regular fa-${isDark ? 'sun' : 'moon'}`}></i>&nbsp;&nbsp;{isDark ? 'Light' : 'Dark'} Mood</p>
     </div>
   </header>
   )
