3a7f755c24c57df52e6b0cbd53180c2975c44d97
sushilsahoo06 <sahoosushil456@gmail.com>
2025-03-04 12:47:59 +0530

    new commit

--- my-app/comonents/Header.jsx
+++ my-app/comonents/Header.jsx
@@ -1,24 +1,23 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import "../style/style.css"
 const Header=()=> {
-  const [isDark ,setIsDark]=useState(()=>{
-    return localStorage.getItem("theme") === "dark"
-  });
-  useEffect(()=>{
-    if(isDark){
-      document.body.classList.add("dark")
-    }else{
-      document.body.classList.remove("dark")
-    }
-    localStorage.setItem("theme" , isDark ? "dark" : "light");
-  },[isDark]);
+  const [isDark ,setIsDark]=useState(JSON.parse(localStorage.getItem('isDark')))
+  // console.log(JSON.parse(localStorage.getItem('isDark')))
+  if(isDark){
+    document.body.classList.add('dark')
+  }else{
+    document.body.classList.remove('dark')
+  }
   return (
     <header className="header">
     <div className="header-content">
       <h2 className="title">Where is the world?</h2>
 
       <p className="theme-change" onClick={()=>
-      setIsDark((prev)=> !prev)
+      {
+        setIsDark(!isDark)
+        localStorage.setItem('isdarkMode',!isDark)
+      }
       }><i className={`fa-regular fa-${isDark ? 'sun' : 'moon'}`}></i>&nbsp;&nbsp;{isDark ? 'Light' : 'Dark'} Mood</p>
     </div>
   </header>
