83bfab9ed97d89c3a08c90ca9f0704e55d3959ff
sushilsahoo06 <sahoosushil456@gmail.com>
2025-03-24 20:42:36 +0530

    new commit

--- forms-situ/src/App.jsx
+++ forms-situ/src/App.jsx
@@ -3,9 +3,12 @@ import './App.css'
 import { ExpenseForm } from './comonents/ExpenseForm'
 import { ExpenseTable } from './comonents/ExpenseTable'
 import expenseData from '../expenseData'
+import { useLocalStorage } from './hooks/useLocalStorage'
 
 function App() {
-  const[expenses,setExpenses]=useState(expenseData)
+  const[expenses,setExpenses]=useState(expenseData);
+  const [localData,setLocalData]=useLocalStorage('mynum',[1,2,3])
+  console.log(localData,setLocalData)
   return (
     <>
       <main>
