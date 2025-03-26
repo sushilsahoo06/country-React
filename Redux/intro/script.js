fe0561086f127fb7f4eb234898ee5f63ee70e329
sushilsahoo06 <sahoosushil456@gmail.com>
2025-03-26 19:53:38 +0530

    new commit

--- Redux/intro/script.js
+++ Redux/intro/script.js
@@ -1,10 +1,44 @@
-let state={
-  count:0
+
+// let prevState=state;
+// function increament(){
+//   //mutating state
+//   // state.count=state.count+1
+//   //not mutating state
+//   state={...state,count:state.count+1}
+// }
+// increament()
+// console.log(state)
+// increament()
+// console.log(state)
+// increament()
+// console.log(state)
+// increament()
+// console.log(state)
+
+let reduxstate = {
+  post: 0,
+  name: "sushil",
+  age: 20
+};
+
+function reducer(state=reduxstate,action) { 
+  if(action.type ==='post/increment'){
+    return { ...state, post: state.post+ 1 };
+  }else if (action.type==='post/decrement'){
+    return { ...state, post: state.post - 1 };
+  }else if (action.type==='post/incrementBy'){
+    return{...state,post:state.post+action.payLoad}
+  }
+  return state
 }
-function increament(){
-  //mutating state
-  // state.count=state.count+1
-  //not mutating state
-  state={count:state.count+1}
-}
-console.log(state)
+console.log(reduxstate);
+reduxstate = reducer(reduxstate,{type:'post/increment'});
+console.log(reduxstate);
+reduxstate = reducer(reduxstate,{type:'post/increment'});
+console.log(reduxstate);
+reduxstate = reducer(reduxstate,{type:'post/decrement'});
+console.log(reduxstate);
+reduxstate = reducer(reduxstate,{type:'post/incrementBy',payLoad:10});
+console.log(reduxstate);
+reduxstate = reducer(reduxstate,{type:'sushil'});
+console.log(reduxstate);
--- Redux/redux_intro/src/App.jsx
+++ Redux/redux_intro/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setCount] = useState({count:0,counterName:'Timer'})
 
   return (
     <>
@@ -18,11 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount({...state,count:state.count+1})}>
+          count is {state.count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR{state.counterName}
         </p>
       </div>
       <p className="read-the-docs">
