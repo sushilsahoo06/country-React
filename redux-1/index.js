d6840e2addd3238252e3c91e1c824004e449ddf9
sushilsahoo06 <sahoosushil456@gmail.com>
2025-06-24 14:55:21 +0530

    new commit

--- redux-1/index.js
+++ redux-1/index.js
@@ -1,17 +1,30 @@
 
-let state={
+
+let reducerstate={
   count:0,
+  name:'sushil',
+  age:'19'
 }
-let prevState=state;
-function increment(){
-  //mutating state
-  // state.count=state.count+1;
-  //non mutating state
-  state={count:state.count+1}
+// let prevState=state;
+// function increment(){
+//   //mutating state
+//   // state.count=state.count+1;
+//   //non mutating state
+//   state={...state,count:state.count+1}
+// }
+
+// increment()
+// console.log(state)
+// increment()
+// console.log(state)
+// increment()
+// console.log(state)
+
+function reducer(state){
+  return{...state,count:state.count+1}
 }
-increment()
-console.log(state)
-increment()
-console.log(state)
-increment()
-console.log(state)
+reducerstate=reducer(reducerstate)
+reducerstate=reducer(reducerstate)
+reducerstate=reducer(reducerstate)
+reducerstate=reducer(reducerstate)
+
