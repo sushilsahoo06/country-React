5a5efe9577d724a30f237344f5995148df7c26c8
sushilsahoo06 <sahoosushil456@gmail.com>
2025-06-25 22:47:08 +0530

    new commit

--- redux_2/src/App.js
+++ redux_2/src/App.js
@@ -1,8 +1,23 @@
+import ProductCard from '../components/Product';
+import productList from '../store/FakeProduct';
+import '../App.css';
 
+export default function App() {
+  console.log(productList);
 
-export default function app() {
   return (
-    <div>app</div>
-  )
+    <div className='products-container'>
+      {productList.map(({ title, rating, price, image }, index) => (
+        <ProductCard
+          key={index}
+          title={title}
+          rating={rating.rate}
+          price={price}
+          imageUrl={image}
+        />
+      ))}
+    </div>
+  );
 }
+
 
--- redux_2/src/main.js
+++ redux_2/src/main.js
@@ -1,9 +1,9 @@
-import {createRoot} from 'react-dom/client'
-import {StrictMode} from 'react'
-import App from './App'
+import { createRoot } from 'react-dom/client'; 
+import { StrictMode } from 'react';     
+import App from './app';                  
 
 createRoot(document.querySelector('#root')).render(
   <StrictMode>
-    <App/>
+    <App />
   </StrictMode>
-)
+);
